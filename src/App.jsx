7ad1a3bfbe0a0aff26bf0cc0883938715b44dc22
mohammad-sowrav <sowravali73@gmail.com>
2023-03-29 21:56:04 +0600

    header creation

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Component/Header/Header'
+
+
+
+function App() {
+  
+    
+  return (
+    <div className="App">
+      <Header></Header>
+     
+    </div>
+  )
+}
+
+export default App
